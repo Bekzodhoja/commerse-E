5914c702f2943e324ffb35a7c3d9538c3e61bc62
Bekzodxoja <100826293+bekzodxoja@users.noreply.github.com>
2023-01-25 00:28:49 +0500

    Addd product Validate

--- resources/views/livewire/admin/admin-add-product-component.blade.php
+++ resources/views/livewire/admin/admin-add-product-component.blade.php
@@ -35,7 +35,7 @@
                                 @if (Session::has('message'))
                                     <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
                                 @endif
-                                <form wire:submit.prevent="storeCategory">
+                                <form wire:submit.prevent="addProduct">
                                     <div class="mb-3 mt-3">
                                         <label for="name" class="form-label">Name</label>
                                         <input type="text" name="name" class="form-control" placeholder="Enter category name" wire:model="name" wire:keyup="genereteSlug"/>
@@ -54,89 +54,92 @@
 
                                     <div class="mb-3 mt-3">
                                         <label for="short_description" class="form-label">Short Description</label>
-                                        <input type="text" name="short_description" class="form-control" placeholder="Enter Short Description" wire:model="slug" />
-                                        @error('slug')
+                                        <input type="text" name="short_description" class="form-control" placeholder="Enter Short Description" wire:model="short_description" />
+                                        @error('short_description')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
                                         <label for="description" class="form-label">Description</label>
-                                        <input type="text" name="description" class="form-control" placeholder="Enter Description" wire:model="slug" />
-                                        @error('slug')
+                                        <input type="text" name="description" class="form-control" placeholder="Enter Description" wire:model="description" />
+                                        @error('description')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
                                         <label for="regular_price" class="form-label">Regular Price</label>
-                                        <input type="text" name="regular_price" class="form-control" placeholder="Enter Regular Price" wire:model="slug" />
-                                        @error('slug')
+                                        <input type="text" name="regular_price" class="form-control" placeholder="Enter Regular Price" wire:model="regular_price" />
+                                        @error('regular_price')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
                                         <label for="sale_price" class="form-label">Sale Price</label>
-                                        <input type="text" name="sale_price" class="form-control" placeholder="Enter Sale Price" wire:model="slug" />
-                                        @error('slug')
+                                        <input type="text" name="sale_price" class="form-control" placeholder="Enter Sale Price" wire:model="sale_price" />
+                                        @error('sale_price')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
                                         <label for="sku" class="form-label">SKU</label>
-                                        <input type="text" name="sku" class="form-control" placeholder="Enter SKU" wire:model="slug" />
-                                        @error('slug')
+                                        <input type="text" name="sku" class="form-control" placeholder="Enter SKU" wire:model="sku" />
+                                        @error('sku')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
-                                        <label for="sku" class="form-label">Stock Status</label>
+                                        <label for="sku" class="form-label" wire:model="stock_status">Stock Status</label>
                                         <select name="" id="" class="form-control">
                                             <option value="instock">InStock</option>
                                             <option value="outofstock">Out Of Stock</option>
                                         </select>
-                                        @error('slug')
+                                        @error('stock_status')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
-                                        <label for="featured" class="form-label">Featured</label>
+                                        <label for="featured" class="form-label" wire:model="featured">Featured</label>
                                         <select name="featured" id="" class="form-control">
                                             <option value="0">No</option>
                                             <option value="1">Yes</option>
                                         </select>
-                                        @error('slug')
+                                        @error('featured')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
                                         <label for="quantity" class="form-label">Quantity</label>
-                                        <input type="text" name="quantity" class="form-control" placeholder="Enter Product Quantity" wire:model="slug" />
-                                        @error('slug')
+                                        <input type="text" name="quantity" class="form-control" placeholder="Enter Product Quantity" wire:model="quantity" />
+                                        @error('quantity')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     <div class="mb-3 mt-3">
-                                        <label for="image" class="form-label">Image</label>
+                                        <label for="image" class="form-label" wire:model="image">Image</label>
                                         <input type="file" name="image" class="form-control" />
-                                        @error('slug')
+                                        @error('image')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
 
                                     
                                     <div class="mb-3 mt-3">
-                                        <label for="category_id" class="form-label">Category</label>
-                                        <select name="category_id" id="" class="form-control">
+                                        <label for="category_id" class="form-label" >Category</label>
+                                        <select name="category_id" id="" class="form-control" wire:model="category_id">
                                             <option value="">Select Category</option>
+                                            @foreach ($categories as $category)
+                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
                                         </select>
-                                        @error('slug')
+                                        @error('category_id')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                     </div>
